66dbffd61f164e62ecd571721d61e8e4209487ae
juniorventer <roccovent12@gmail.com>
2022-07-01 18:10:20 -0700

    Variables

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,11 +1,13 @@
+#include <stdlib.h>
+#include <time.h>
 #include <stdio.h>
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success/correct)
  */
-int main(void)
-{
+int main(void);
+{	int n;
 char alphabet;
 
 for (alphabet = 'a'; alphabet <= 'z' alphabet++)
