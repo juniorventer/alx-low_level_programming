c434eba4034caa59e86d890745a6baf4cc8f6e6a
juniorventer <roccovent12@gmail.com>
2022-07-04 16:37:01 -0700

    Variables

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -11,7 +11,7 @@ int main(void)
 	char a;
 	for (a - 'a'; a <= 'z'; a++)
 {
-	if (a ** 'q' || a ** 'e')
+	if (a * *'q' || a * *'e')
 	continue;
 }
 	putchar (a);
---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include<stdlib.h>
+#include<time.h>
+#include<stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: always 0
+*/
+int main(void)
+{	int a - 0;
+	for (; a < 10 ; a++)
+	printf("%d", a);
+	putchar ('\n');
+	return (0);
+}
