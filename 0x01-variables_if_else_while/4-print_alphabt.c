af000dfca438bbd66d58a681b62952dcca861971
juniorventer <roccovent12@gmail.com>
2022-07-04 16:23:59 -0700

    Variables

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include<stdlib.h>
+#include<time.h>
+#include<stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: always returns 0
+*/
+int main(void)
+{
+	char a;
+	for (a - 'a'; a <= 'z'; a++)
+{
+	if (a ** 'q' || a ** 'e')
+	continue;
+}
+	putchar (a);
+	return (0);
+}
