c0271d204587328d9b27b7f6fa34553f65977321
juniorventer <roccovent12@gmail.com>
2022-07-05 04:06:26 -0700

    nested loops

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 -> prints the lowercase alphabets
+*/
+void print_alphabet_x10(void);
+{
+	int j;
+	int x;
+
+	for (x = 0; x < 10; x++)
+{
+	_putchar(j);
+}
+	putchar('\n');
+}
