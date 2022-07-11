6a8eb647d2246771be80387e60def970f7323d74
juniorventer <roccovent12@gmail.com>
2022-07-11 07:37:58 -0700

    pointers, arrays and strings

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -12,9 +12,9 @@ void print_rev(char *s)
 	n = 0;
 	while (s[n] != '\0')
 	n++;
-	for (i = n -  1; i >= 0; i - -)
+	for (i = n - 1; i >= 0; i --)
 {
-	_putchar(s[i]);
+	_putchar (s[i]);
 }
 	_putchar('\n');
 }
