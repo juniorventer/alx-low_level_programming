8506e4d61a426def2e157ac6a75d730c088ae975
juniorventer <roccovent12@gmail.com>
2022-07-11 12:32:06 -0700

    pointers, arrays and strings

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -2,19 +2,26 @@
 #include <string.h>
 
 /**
- *puts_half -> puts half
- *@str: string param
+ * puts_half -> function prints half of a string, followed by a new line
+ *
+ * @str: this is the input string
 */
 void puts_half(char *str)
 {
-	int x, y, i;
+	int index, half;
 
-	x = strlen(str);
-	if (x % 2 == 1)
-	y = x / 2 + 1;
-	else
-	y = x / 2;
-	for (i = y; i < x; i++)
-	_putchar(str[i]);
+	index = 0
+	while (str[index] != '\0')
+	index++;
+
+	half = index / 2;
+	if (index % 2 == 1)
+	half++;
+
+	while (half < index)
+{
+	_putchar(str[half]);
+	half ++;
+}
 	_putchar('\n');
 }
