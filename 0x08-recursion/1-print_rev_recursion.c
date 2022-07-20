c832dd977bddc4359b6477214ba70b94206c4cd5
juniorventer <roccovent12@gmail.com>
2022-07-20 04:39:06 -0700

    Recursion

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -10,11 +10,8 @@ void _print_rev_recursion(char *s)
 {
 	if (*s == '\0')
 {
-	return ();
-}
-
-	s++;
-	_print_rev_recursion(s);
-	s--;
+	return;
+{
+	_print_rev_recursion(s + 1);
 	_putchar(*s);
 }
