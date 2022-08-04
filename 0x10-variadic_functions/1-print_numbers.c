352e9d7ee75df01d59f66d3491a24e742a59094e
juniorventer <roccovent12@gmail.com>
2022-08-04 12:15:30 -0700

    Variadic functions

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -9,7 +9,7 @@
  * Description: prints numbers, followed by a new line
  * Return: na
 */
-void print_numbers(const char *separator, const unsigned int n, ...)
+void print_numbers(const char *separator, const unsigned int n, ...);
 {
 	unsigned int i;
 	va_list ap;
